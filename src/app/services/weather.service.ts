17bd63295f0e8b126641e7405e331a6534cf7476
Reynaldo <reynaldozig@hotmail.com>
2019-03-07 06:40:38 -0500

    Update weather.service.ts

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class WeatherService {
   // ${zipCode}
-  apiKey = '0192f6164e1b4d5dcd57ce9cebfc7921';
+  
   city = 'Norcross';
   ApiURL = `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}`;
   constructor(private http: HttpClient) { }
